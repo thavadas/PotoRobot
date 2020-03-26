d89e7484ce6258683779a73921fed17d281d9f99
thavadas <thavadas@tutanota.com>
2020-03-26 15:18:45 +0100

    Updates

---
+++ precache-manifest.2117a4f9b9d18a18fc0fe2f6cca76168.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5048ecf29326012e19276266b0782cd0",
+    "url": "/PotoRobot/index.html"
+  },
+  {
+    "revision": "5e3dabe0407439aeaf40",
+    "url": "/PotoRobot/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "a093faf23a355ebcb5f2",
+    "url": "/PotoRobot/static/css/main.5aec315b.chunk.css"
+  },
+  {
+    "revision": "5e3dabe0407439aeaf40",
+    "url": "/PotoRobot/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/PotoRobot/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a093faf23a355ebcb5f2",
+    "url": "/PotoRobot/static/js/main.8cd60d98.chunk.js"
+  },
+  {
+    "revision": "5012dc899f7700b62b57",
+    "url": "/PotoRobot/static/js/runtime-main.69f480ed.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PotoRobot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
